01c628165875ac5d56bbeecb56db4112cbca36cb
xue jiang <example@imx.com>
2023-03-14 22:20:27 +0800

    contact us

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { SelectedPage } from "@/shared/types";
 import Home from "@/scenes/home";
 import Benefits from "@/scenes/benefits";
 import OurClasses from "@/scenes/ourClasses";
+import ContactUs from "./scenes/contactUs";
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Home
@@ -32,6 +33,7 @@ function App() {
       <Home setSelectedPage={setSelectedPage}></Home>
       <Benefits setSelectedPage={setSelectedPage}></Benefits>
       <OurClasses setSelectedPage={setSelectedPage}></OurClasses>
+      <ContactUs setSelectedPage={setSelectedPage}></ContactUs>
     </div>
   );
 }
